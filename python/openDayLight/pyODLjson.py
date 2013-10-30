0e8ae63dec2eea2ed8b624893d610cd91eb4abba
mikeoutland-nexusis <mike.outland@nexusis.com>
2013-10-29 21:01:13 -0700

    changed file structure, added ODL simple test script.  pyODLjson.py modified to return all switch ID's on a given controller

--- python/openDayLight/pyODLjson.py
+++ python/openDayLight/pyODLjson.py
@@ -9,7 +9,8 @@ password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
 top_level_url = url
 user = 'admin'
 password = 'admin'
-url = url + 'statistics/default/flow'
+flow = url + 'statistics/default/flow'
+switchId = list()
 
 password_mgr.add_password(None, top_level_url, user, password)
 
@@ -18,6 +19,14 @@ handler = urllib2.HTTPBasicAuthHandler(password_mgr)
 opener = urllib2.build_opener(handler)
 urllib2.install_opener(opener)
 
-data = json.load(urllib2.urlopen(url))
+data = json.load(urllib2.urlopen(flow))
 
-print data
+
+def getSwitchIds(dataDict):
+	mylist = dataDict['flowStatistics']
+	for i in range(len(mylist)):
+		mylist1 =  mylist[i]
+		switchId.insert(i, mylist1['node']['id'])
+	return switchId
+
+print (getSwitchIds(data))
