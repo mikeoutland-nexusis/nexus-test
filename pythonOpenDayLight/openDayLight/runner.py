f4d35e232ffdf37d29a218aa341042da5e9065a0
mikeoutland-nexusis <mike.outland@nexusis.com>
2013-11-26 15:38:24 -0800

    review test again part-two
    Change-Id: I7e96478c9bfb3fcff4c26efa671a49b7d3e367b4

--- pythonOpenDayLight/openDayLight/runner.py
+++ pythonOpenDayLight/openDayLight/runner.py
@@ -6,6 +6,7 @@ Created on Nov 14, 2013
 
 #In mininet i'm using the following config:
 #    mn --topo single,3 --mac --switch ovsk --controller=remote,ip=10.0.2.2,port=6633
+#review this comment.
 
 from pythonOpenDayLight.openDayLight import odlSwitch
 
