6cd4066963a946fea23f02ee9c4923b9a930a20b
mikeoutland-nexusis <mike.outland@gmail.com>
2013-12-09 15:38:23 -0800

    Updated .sh file permissions to execute so jenkins can handle them properly.
    Change-Id: Iaa717616986936349c311014a6b38eef0c6c66d1

--- scripts/test/sendMininetCmd.sh
+++ scripts/test/sendMininetCmd.sh
@@ -2,4 +2,4 @@
 
 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $3 "export TERM=xterm-256color && /home/ubuntu/runCmd.sh $1 $2"
 echo "connecting to mininet to execute: /home/ubuntu/runCmd.sh $1 $2 at $3"
-sleep 5
+sleep 10
