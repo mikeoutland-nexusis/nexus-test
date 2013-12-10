50465941a150fd51d4ba993f2f54184709626484
Colin McNamara <colin@2cups.com>
2013-12-10 07:12:15 -0800

    added demo'd version files
    Change-Id: Ie5cd52e5827b72a41a4d520209c63e6e00d7c54f

--- scripts/test/startMininet.3sw2h.sh
+++ scripts/test/startMininet.3sw2h.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Change the --topo varable to reflect the topology class you are running
 # Current choices available
-#   T   
+#   TwoSwFourHostTopo
 #   ThreeSwTwoHostPerTopo
 # Don't forget to change runner file to reflect topo changes
 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "/home/ubuntu/runSys.sh $1 --custom mininet/custom/topo3Sw2HostPer.py --topo ThreeSwTwoHostPerTopo"
