d07d2296962c303890540964add10e0160fe31bb
mikeoutland-nexusis <mike.outland@gmail.com>
2013-12-09 18:10:44 -0800

    again
    Change-Id: I9c929d8231075580571f399e7cf9e75bc98c4b11

--- scripts/test/runMininetTest.sh
+++ scripts/test/runMininetTest.sh
@@ -4,23 +4,27 @@ ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256col
 sleep 2
 echo $?
 if [ "$?" -ne "0" ]
+then
 	exit 1
 fi
 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h1 h3"
 sleep 2
 echo $?
-if [ "$?" -ne "0" ]
+iif [ "$?" -ne "0" ]
+then
 	exit 1
 fi
 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h4 h1"
 sleep 2
 echo $?
-iif [ "$?" -ne "0" ]
+if [ "$?" -ne "0" ]
+then
 	exit 1
 fi
 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h4 h6"
 sleep 2
 echo $?
 if [ "$?" -ne "0" ]
+then
 	exit 1
 fi
