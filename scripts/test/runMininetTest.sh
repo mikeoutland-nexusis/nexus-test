2ae2c633a1f9375be2e158a94c8c89babf4e8f99
mikeoutland-nexusis <mike.outland@gmail.com>
2013-12-09 18:06:01 -0800

    2nd bad test, should fail
    Change-Id: Ia7d59892a7464cf56a76e1b5227db6fe454b8eb3

--- scripts/test/runMininetTest.sh
+++ scripts/test/runMininetTest.sh
@@ -3,12 +3,24 @@
 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h1 h2"
 sleep 2
 echo $?
+if [[ $? != 0]]
+	exit 1
+fi
 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h1 h3"
 sleep 2
 echo $?
+if [[ $? != 0]]
+	exit 1
+fi
 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h4 h1"
 sleep 2
 echo $?
+if [[ $? != 0]]
+	exit 1
+fi
 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h4 h6"
 sleep 2
 echo $?
+if [[ $? != 0]]
+	exit 1
+fi
