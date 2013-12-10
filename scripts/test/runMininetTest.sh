8e346b6cc191e6945339b37683a6017dc8e75b4e
mikeoutland-nexusis <mike.outland@gmail.com>
2013-12-09 17:58:19 -0800

    Testing a bad test, should fail
    Change-Id: I5874b01bb081107cdaa52b23fbdf1347fd8b4207

--- scripts/test/runMininetTest.sh
+++ scripts/test/runMininetTest.sh
@@ -2,9 +2,13 @@
 
 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h1 h2"
 sleep 2
+echo $?
 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h1 h3"
 sleep 2
-ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h4 h5"
+echo $?
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h4 h1"
 sleep 2
+echo $?
 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h4 h6"
 sleep 2
+echo $?
