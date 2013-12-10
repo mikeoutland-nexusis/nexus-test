8413efb09cb653dfd7b308c70c8888dea708e922
mikeoutland-nexusis <mike.outland@gmail.com>
2013-12-09 17:54:05 -0800

    updated ping test
    Change-Id: I4dff5c80f26fe11f8202da4be3dc4cf535d18650

--- scripts/test/remote/runPingTest.sh
+++ scripts/test/remote/runPingTest.sh
@@ -5,19 +5,15 @@ set h2 [lindex $argv 2]
 
 eval spawn screen -r $name
 expect "*** Starting CLI:"
-send $h1 ping -c 1 $h2 | grep transmitted\r
-expect { 
-	"1 packets transmitted, 1 received, 0% packet loss, time 0ms" 
-	{
-		send ctrl+d\r
-		expect eof
-		exit 0
-	}
-	{ 
-	"1 packets transmitted, 0 received, 100% packet loss, time 0mx" 
-	{ 
-		send ctrl+d\r
-		expect eof
-		exit 1 
-	} 
+send "$h1 ping -c 1 $h2 | grep transmitted\n"
+expect {
+        "1 packets transmitted, 1 received, 0% packet loss, time 0ms" {
+                send ctrl+d\r
+                expect eof
+                exit 0
+        }
+
+        "1 packets transmitted, 0 received, +1 errors, 100% packet loss, time 0ms" {
+                exit 1
+        }
 }
