66dcdcacc1c60c93ecef6f6bcc35f7a69f977c66
mikeoutland-nexusis <mike.outland@gmail.com>
2013-12-09 21:18:59 -0800

    should fail
    Change-Id: I0a70fb4f42a6510a7e3208822ddfd0bf47feb0bb

--- scripts/test/runMininetTest.sh
+++ scripts/test/runMininetTest.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "(export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h1 h2) || exit -1"
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h1 h2"
+if [ $? -ne 0 ]
+then
+	exit 1
+fi
 sleep 2
-ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "(export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h1 h3) || exit -1"
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "(export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h1 h3) || exit -1"
+if [ $? -ne 0 ]
+then
+	exit 1
+fi
 sleep 2
-ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "(export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h4 h1) || exit -1"
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "(export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h4 h1) || exit -1"
+if [ $? -ne 0 ]
+then
+	exit 1
+fi
 sleep 2
-ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "(export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h4 h6) || exit -1"
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "(export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h4 h6) || exit -1"
+if [ $? -ne 0 ]
+then
+	exit 1
+fi
 sleep 2
