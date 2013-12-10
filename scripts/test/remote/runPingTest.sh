90aa545034972055cd2bc03e885dac77cb67ca03
mikeoutland-nexusis <mike.outland@gmail.com>
2013-12-09 20:12:26 -0800

    should fail
    Change-Id: I2d14bed878d718dc6de84b1f6fe9b5d712861105

--- scripts/test/remote/runPingTest.sh
+++ scripts/test/remote/runPingTest.sh
@@ -4,7 +4,6 @@ set h1 [lindex $argv 1]
 set h2 [lindex $argv 2]
 
 eval spawn screen -r $name
-expect "*** Starting CLI:"
 send "$h1 ping -c 1 $h2 | grep transmitted\n"
 expect {
         "1 packets transmitted, 1 received, 0% packet loss, time 0ms" {
@@ -12,8 +11,5 @@ expect {
                 expect eof
                 exit 0
         }
-
-        "1 packets transmitted, 0 received, +1 errors, 100% packet loss, time 0ms" {
-                exit 1
-        }
 }
+exit -1
--- scripts/test/runMininetTest.sh
+++ scripts/test/runMininetTest.sh
@@ -3,32 +3,32 @@
 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h1 h2"
 sleep 2
 echo $?
-if [ "$?" -eq "69" ]
+if [ "$?" -ne "0" ]
 then
 	echo 'error in test' > /dev/stderr
-	exit 69
+	exit -1
 fi
 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h1 h3"
 sleep 2
 echo $?
-if [ "$?" -eq "69" ]
+if [ "$?" -ne "0" ]
 then
 	echo 'error in test' > /dev/stderr
-	exit 69
+	exit -1
 fi
 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h4 h1"
 sleep 2
 echo $?
-if [ "$?" -eq "69" ]
+if [ "$?" -ne "0" ]
 then
 	echo 'error in test' > /dev/stderr
-	exit 69
+	exit -1
 fi
 ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h4 h6"
 sleep 2
 echo $?
-if [ "$?" -eq "69" ]
+if [ "$?" -ne "0" ]
 then
 	echo 'error in test' > /dev/stderr
-	exit 69
+	exit -1
 fi
