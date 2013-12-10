bf1badaa25f2daef01b69e562a2514c311497490
mikeoutland-nexusis <mike.outland@gmail.com>
2013-12-09 21:46:58 -0800

    updated test cases
    Change-Id: Ib67ceceb6a719dd3336532a6b1e0ac22bb6191f4

--- scripts/test/runMininetTest.sh
+++ scripts/test/runMininetTest.sh
@@ -1,26 +1,63 @@
 #!/bin/bash
 
-ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "(export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h1 h2) || exit -1"
-if [ $? -ne 0 ]
-then
-	exit 1
-fi
+echo "Positive Testcases:"
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h1 h2"
 sleep 1
-ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "(export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h1 h6) || exit -1"
-if [ $? -ne 0 ]
-then
-	exit 1
-fi
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h1 h3"
 sleep 1
-ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "(export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h4 h5) || exit -1"
-if [ $? -ne 0 ]
-then
-	exit 1
-fi
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h2 h1"
 sleep 1
-ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "(export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h4 h6) || exit -1"
-if [ $? -ne 0 ]
-then
-	exit 1
-fi
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h2 h3"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h3 h1"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h3 h2"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h4 h5"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h4 h6"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h5 h6"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h5 h4"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h6 h4"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h6 h5"
+echo "Negative Testcases"
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h1 h4"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h1 h5"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h1 h6"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h2 h4"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h2 h5"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h2 h6"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h3 h4"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h3 h5"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h3 h6"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h4 h1"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h4 h2"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h4 h3"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h5 h1"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h5 h2"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h5 h3"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h6 h1"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h6 h2"
+sleep 1
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && exec /home/ubuntu/runPingTest.sh $1 h6 h3"
 sleep 1
