d890863aee61ea6242831cb2a23a19edd3885b18
Colin McNamara <colin@2cups.com>
2013-12-10 07:45:33 -0800

    adding startmininet shell script version to setup a 2 sw topo
    Change-Id: I4da22d11d3df84ba0b05ecbbab76458e1c40807e

---
+++ scripts/test/startMininet.2sw2h.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Change the --topo varable to reflect the topology class you are running
+# Current choices available
+#   TwoSwFourHostTopo
+#   ThreeSwTwoHostPerTopo
+#   twoSwTwoHostPerTopo
+# Don't forget to change runner file to reflect topo changes
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "/home/ubuntu/runSys.sh $1 --custom mininet/custom/topo2Sw2Host.py --topo ThreeSwTwoHostPerTopo"
+echo "connecting to mininet to execute: /home/ubuntu/runSys.sh $1 at $2"
+sleep 5 
