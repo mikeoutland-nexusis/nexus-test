04e2f687fbfd8ba79906a7bc5bbf27841b1bbed7
mikeoutland-nexusis <mike.outland@gmail.com>
2013-12-09 20:34:10 -0800

    another
    Change-Id: I0b9599386f2a2a41a886e65d4e34b849884e1087

--- scripts/test/runMininetTest.sh
+++ scripts/test/runMininetTest.sh
@@ -1,34 +1,10 @@
 #!/bin/bash
 
-ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h1 h2"
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "(export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h1 h2) || exit -1"
 sleep 2
-echo $?
-if [ "$?" -ne "0" ]
-then
-	echo 'error in test' > /dev/stderr
-	exit -1
-fi
-ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h1 h3"
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "(export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h1 h3) || exit -1"
 sleep 2
-echo $?
-if [ "$?" -ne "0" ]
-then
-	echo 'error in test' > /dev/stderr
-	exit -1
-fi
-ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h4 h1"
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "(export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h4 h1) || exit -1"
 sleep 2
-echo $?
-if [ "$?" -ne "0" ]
-then
-	echo 'error in test' > /dev/stderr
-	exit -1
-fi
-ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h4 h6"
+ssh -i ~/.ssh/id_rsa -o StrictHostKeyChecking=false $2 "(export TERM=xterm-256color && /home/ubuntu/runPingTest.sh $1 h4 h6) || exit -1"
 sleep 2
-echo $?
-if [ "$?" -ne "0" ]
-then
-	echo 'error in test' > /dev/stderr
-	exit -1
-fi
