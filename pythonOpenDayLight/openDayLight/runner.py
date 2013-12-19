9665ade38f1b02e8ce69d34417f9f9df90cfbb63
mikeoutland-nexusis <mike.outland@gmail.com>
2013-12-19 12:15:02 -0800

    comment to trigger
    Change-Id: I586b10b1e43ff197c8b85a97f5474e437e696055

--- pythonOpenDayLight/openDayLight/runner.py
+++ pythonOpenDayLight/openDayLight/runner.py
@@ -6,6 +6,7 @@ Created on Nov 14, 2013
 
 #In mininet i'm using the following config:
 #    mn --custom custom/topo3Sw2HostPer.py --topo ThreeSwTwoHostPerTopo --mac --switch ovsk --controller=remote,ip=10.0.2.2,port=6633
+#adding comment to trigger
 
 import socket
 import sys
